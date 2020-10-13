ee67e9c68e36688c231e8f3168fdf35e0b66df39
sarwar_sikder <amin.se.aiub@gmail.com>
2020-10-13 14:01:00 +0600

    js basic

---
+++ script.js
@@ -0,0 +1,70 @@
+"use strict"
+
+/*var person = [
+				[
+					[12,34,55],
+					[12,34,55]
+				],
+				[
+					[33,56,46],
+					[22,43,77]
+				]
+			];
+
+console.log(person[1][1][2]);*/
+
+
+function sum(a=6, b=0){
+	return a+b;
+}
+
+var b = function(a=6, b=0){
+	return a+b;
+}
+
+var c = (a=10, b=0)=>{
+	return a+b;
+}
+
+var student = {
+	sid: '11-11-11',
+	sname: 'alamin',
+	sdept: 'SE',
+	getName: function(){
+		return this.sname;
+	}
+};
+
+var student2 = {
+	sid: '10-10-10',
+	sname: 'XYZ',
+	sdept: 'SE',
+	getName: function(){
+		return {
+			scgpa: 4,
+			getCgpa: function(){
+				return this.scgpa;
+			}
+		}
+	}
+};
+
+student.cgpa = 4;
+
+//alert(student2.getName().getCgpa());
+//alert(student2['getName']()['getCgpa']());
+//alert(student2.sname);
+
+
+function person(name, age){
+	this.name = name;
+	this.age = age;
+}
+
+
+var s1 = new person('alamin', '12');
+var s2 = new person('xyz', '123');
+var s3 = new person('pgr', '44');
+
+alert(s1.name);
+
