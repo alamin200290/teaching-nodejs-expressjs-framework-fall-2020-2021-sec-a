54ec41d6e5f78f709f909e8f0c31ac9399c72d38
sarwar_sikder <amin.se.aiub@gmail.com>
2020-10-27 13:19:07 +0600

    view engine basic+data passing to view

--- node/expressjs/app.js
+++ node/expressjs/app.js
@@ -1,16 +1,28 @@
+//declaration
 const express 	= require('express');
+//const ejs		= require('ejs');
 const app 		= express();
 
+//config
+app.set('view engine', 'ejs');
+
+//middleware
+
+
+//route
 app.get('/', (request, response)=>{
 	console.log('requested url /');
-	response.send('<h1>Express web server works...</h1>');
+	//response.send('home.html', {name: alamin});
 });
 
 app.get('/home', (request, response)=>{
-	console.log('requested url /home');
-	response.send('Home page');
-})
 
+	var user = {name: 'XYZ', id: '12112'};
+	response.render('home/index', user);
+});
+
+
+//server startup
 app.listen(3000, (error)=> {
 	console.log('express server started at 3000 port');
 });
